3bd60a4da32193adf1e0547fb3a924a1ac277ade
Joseph Schultz <jschultz333@gmail.com>
2021-06-13 15:06:03 -0500

    Smooth scrolling for nav items

--- js/app.js
+++ js/app.js
@@ -126,17 +126,53 @@ function treeScrolling() {
     $('.mountains').css('bottom','calc(-20px + '+ mountains +'px)');
 }
 
-document.onreadystatechange = () => {
+// document.onreadystatechange = () => {
   
-    if (document.readyState === 'complete') {
+//     if (document.readyState === 'complete') {
               
-      /**
-       * Setup your Lazy Line element.
-       * see README file for more settings
-       */
+//       /**
+//        * Setup your Lazy Line element.
+//        * see README file for more settings
+//        */
 
-      let el = document.querySelector('#sofia');
-      let myAnimation = new LazyLinePainter(el, {"ease":"easeInQuad","strokeWidth":2,"strokeOpacity":1,"strokeColor":"#682714","strokeCap":"round"}); 
-      myAnimation.paint(); 
+//       let el = document.querySelector('#sofia');
+//       let myAnimation = new LazyLinePainter(el, {"ease":"easeInQuad","strokeWidth":2,"strokeOpacity":1,"strokeColor":"#682714","strokeCap":"round"}); 
+//       myAnimation.paint(); 
+//     }
+// }
+
+$('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+    // On-page links
+    if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+    ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+            } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+            };
+        });
+        }
     }
-  }
+});
