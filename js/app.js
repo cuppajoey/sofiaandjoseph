799fd44174257d3ed2ea31abe100afc8379bc6db
Joseph Schultz <jschultz333@gmail.com>
2021-06-13 12:15:28 -0500

    Add line animation assets

--- js/app.js
+++ js/app.js
@@ -1,107 +1,107 @@
-particlesJS('starry-night', {
-    "particles": {
-        "number": {
-            "value": 80,
-            "density": {
-                "enable": true,
-                "value_area": 900
-            }
-        },
-        "color": {
-            "value": "#233B45"
-        },
-        "shape": {
-            "type": [
-                "circle",
-                "image",
-            ],
-            "image": {
-                "src": "http://cuppajoey.com/shareimg/star.png",
-                "width": 15,
-                "height": 15
-            }
-        },
-        "size": {
-            "value": 5,
-            "random": true,
-            "anim": {
-                "enable": false,
-                "speed": 10,
-                "size_min": 0.1,
-                "sync": false
-            }
-        },
-        "line_linked": {
-            "enable": false,
-            "distance": 150,
-            "color": "#ffffff",
-            "opacity": 0.20844356791251797,
-            "width": 1
-        },
-        "move": {
-            "enable": true,
-            "speed": 0.2,
-            "direction": "none",
-            "random": false,
-            "straight": false,
-            "out_mode": "out",
-            "bounce": false,
-            "attract": {
-                "enable": false,
-                "rotateX": 600,
-                "rotateY": 1200
-            }
-        }
-    },
-    "interactivity": {
-        "detect_on": "canvas",
-        "events": {
-        "onhover": {
-            "enable": false,
-            "mode": "repulse"
-        },
-        "onclick": {
-            "enable": false,
-            "mode": "push"
-        },
-        "resize": false
-        },
-        "modes": {
-        "grab": {
-            "distance": 400,
-            "line_linked": {
-            "opacity": 1
-            }
-        },
-        "bubble": {
-            "distance": 400,
-            "size": 40,
-            "duration": 2,
-            "opacity": 8,
-            "speed": 3
-        },
-        "repulse": {
-            "distance": 200,
-            "duration": 0.4
-        },
-        "push": {
-            "particles_nb": 4
-        },
-        "remove": {
-            "particles_nb": 2
-        }
-        }
-    },
-    "retina_detect": false,
-    "config_demo": {
-        "hide_card": false,
-        "background_color": "#b61924",
-        "background_image": "",
-        "background_position": "50% 50%",
-        "background_repeat": "no-repeat",
-        "background_size": "cover"
-    }
-});
+// particlesJS('starry-night', {
+//     "particles": {
+//         "number": {
+//             "value": 80,
+//             "density": {
+//                 "enable": true,
+//                 "value_area": 900
+//             }
+//         },
+//         "color": {
+//             "value": "#233B45"
+//         },
+//         "shape": {
+//             "type": [
+//                 "circle",
+//                 "image",
+//             ],
+//             "image": {
+//                 "src": "http://cuppajoey.com/shareimg/star.png",
+//                 "width": 15,
+//                 "height": 15
+//             }
+//         },
+//         "size": {
+//             "value": 5,
+//             "random": true,
+//             "anim": {
+//                 "enable": false,
+//                 "speed": 10,
+//                 "size_min": 0.1,
+//                 "sync": false
+//             }
+//         },
+//         "line_linked": {
+//             "enable": false,
+//             "distance": 150,
+//             "color": "#ffffff",
+//             "opacity": 0.20844356791251797,
+//             "width": 1
+//         },
+//         "move": {
+//             "enable": true,
+//             "speed": 0.2,
+//             "direction": "none",
+//             "random": false,
+//             "straight": false,
+//             "out_mode": "out",
+//             "bounce": false,
+//             "attract": {
+//                 "enable": false,
+//                 "rotateX": 600,
+//                 "rotateY": 1200
+//             }
+//         }
+//     },
+//     "interactivity": {
+//         "detect_on": "canvas",
+//         "events": {
+//         "onhover": {
+//             "enable": false,
+//             "mode": "repulse"
+//         },
+//         "onclick": {
+//             "enable": false,
+//             "mode": "push"
+//         },
+//         "resize": false
+//         },
+//         "modes": {
+//         "grab": {
+//             "distance": 400,
+//             "line_linked": {
+//             "opacity": 1
+//             }
+//         },
+//         "bubble": {
+//             "distance": 400,
+//             "size": 40,
+//             "duration": 2,
+//             "opacity": 8,
+//             "speed": 3
+//         },
+//         "repulse": {
+//             "distance": 200,
+//             "duration": 0.4
+//         },
+//         "push": {
+//             "particles_nb": 4
+//         },
+//         "remove": {
+//             "particles_nb": 2
+//         }
+//         }
+//     },
+//     "retina_detect": false,
+//     "config_demo": {
+//         "hide_card": false,
+//         "background_color": "#b61924",
+//         "background_image": "",
+//         "background_position": "50% 50%",
+//         "background_repeat": "no-repeat",
+//         "background_size": "cover"
+//     }
+// });
 
 $(window).scroll(function() {
     imageScrollingStrip();
@@ -117,9 +117,26 @@ function treeScrolling() {
     var foreground = $(window).scrollTop() * 0.25;
     var midground = $(window).scrollTop() * 0.1;
     var background = $(window).scrollTop() * 0.05;
+    var mountains = $(window).scrollTop() * 0.03;
     $('.t-one').css('bottom','calc(-200px + '+ foreground +'px)');
     $('.t-two').css('bottom','calc(-125px + '+ foreground +'px)');
     $('.t-three').css('bottom','calc(-85px + '+ midground +'px)');
     $('.t-four').css('bottom','calc(-85px + '+ midground +'px)');
     $('.t-five').css('bottom','calc(-40px + '+ background +'px)');
+    $('.mountains').css('bottom','calc(-20px + '+ mountains +'px)');
 }
+
+document.onreadystatechange = () => {
+  
+    if (document.readyState === 'complete') {
+              
+      /**
+       * Setup your Lazy Line element.
+       * see README file for more settings
+       */
+
+      let el = document.querySelector('#sofia');
+      let myAnimation = new LazyLinePainter(el, {"ease":"easeInQuad","strokeWidth":2,"strokeOpacity":1,"strokeColor":"#682714","strokeCap":"round"}); 
+      myAnimation.paint(); 
+    }
+  }
