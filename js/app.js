35c3361dd567f372424fc9bb27d4224495c9c126
Joseph Schultz <jschultz333@gmail.com>
2021-09-21 22:38:52 -0500

    Add javascript countdown timer

--- js/app.js
+++ js/app.js
@@ -176,3 +176,41 @@ $('a[href*="#"]')
         }
     }
 });
+
+// document.addEventListener("DOMContentLoaded", function(){
+	var countdown = document.getElementById("jdsc_countdown");
+
+	if (countdown) {
+	    document.getElementById("jdsc_days").innerHTML = '12';
+        // Set the default date we're counting down to
+        var countDownDate = new Date("November 28, 2021").getTime();
+
+        // Update the count down every 1 second
+        var x = setInterval(function() {
+
+            // Get todays date and time
+            var now = new Date().getTime();
+
+            // Find the distance between now and the count down date
+            var distance = countDownDate - now;
+
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            if (distance > 0) {
+                document.getElementById("jdsc_days").innerHTML = days;
+                document.getElementById("jdsc_hours").innerHTML = hours;
+                document.getElementById("jdsc_min").innerHTML = minutes;
+                document.getElementById("jdsc_sec").innerHTML = seconds;
+                countdown.classList.add("loaded");
+            } else {
+                clearInterval(x);
+            }
+        }, 1000);
+	} else {
+		console.log('Cannot find Timer');
+	}
+// });
